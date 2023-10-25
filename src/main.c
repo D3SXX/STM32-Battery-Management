611aa9e4a32ffe723c0354b30b8fa76fd1a79fcf
Siyuan Xu <siyuan.xu@outlook.com>
2023-10-25 14:06:16 +0300

    move modbus_routine out from interrupt to main

--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@
 #include "sysclock_config.h"
 #include "usart_config.h"
 #include "utils.h"
+#include "adc.h"
 
 #define DEBUG_CONSOLE_EN 1
 
@@ -13,9 +14,17 @@
 #include "stdio.h"
 #endif
 
-/* Private define  */
-bool u1tcFlag = false;  // USART1 Transmite complete flag
-bool u1tsFlag = false;  // USART1 Transmite started flag
+/* Private flags  */
+#define MODBUS_REQUEST       0x01
+#define ADC_READ_CURRENT     0x02
+#define ADC_READ_TEMPERATURE 0x04
+#define ADC_READ_VOLTS       0x08
+#define ADC_READ_ALL         0x0E
+#define DMA1_ERROR           0x8000
+
+uint16_t subroutine_flag = 0x00;
+bool     u1ts_flag       = false;  // USART1 Transmite started flag
+bool     u1tc_flag       = false;  // USART1 Transmite complete flag
 
 /* Private macro */
 #define ARRAY_LEN(x) (sizeof(x) / sizeof((x)[0]))
@@ -24,7 +33,7 @@ bool u1tsFlag = false;  // USART1 Transmite started flag
 _Atomic uint8_t usart1_rx_dma_buffer[USART1_RX_DMA_BUFFER_SIZE];
 _Atomic uint8_t usart2_rx_dma_buffer[USART2_RX_DMA_BUFFER_SIZE];
 
-/* Private define  */
+/* Private function prototypes  */
 static inline void LED2_init(void) {
     RCC->AHBENR |= 1;       // GPIOA ABH bus clock ON. p154
     GPIOA->MODER |= 0x400;  // GPIOA pin 5 to output. p184
@@ -34,6 +43,8 @@ void Configure_DebugPin(uint32_t debug_pin) {
     LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOA);
     LL_GPIO_SetPinMode(GPIOA, debug_pin, LL_GPIO_MODE_OUTPUT);
 }
+
+void modbus_routine();
 
 /* Main routine */
 int main(void) {
@@ -47,12 +58,30 @@ int main(void) {
     debug_console("App started...\n\r");
     LL_GPIO_SetPinMode(GPIOA, LL_GPIO_PIN_8, LL_GPIO_MODE_OUTPUT);  // config a debug pin on GPIO
     __enable_irq();
+
     while (1) {
         IWDG_feed();
-        // debug_console("app running...\n\r");
-        delay_ms(1000);
-        LL_GPIO_TogglePin(GPIOA, LL_GPIO_PIN_5);
+
+        if (subroutine_flag & ADC_READ_CURRENT) {
+            adc_read_current(modbus_registers + REG_ADDR_BATT_CURRENT);
+        }
+        if (subroutine_flag & ADC_READ_TEMPERATURE) {
+            adc_read_temperature(modbus_registers + REG_ADDR_BATT_CURRENT);
+        }
+        if (subroutine_flag & ADC_READ_VOLTS) {
+            adc_read_volts(modbus_registers + REG_ADDR_BATT_CURRENT);
+        }
+        if (subroutine_flag & MODBUS_REQUEST) {
+            modbus_routine();
+        }
+        if (subroutine_flag & DMA1_ERROR) {
+            USART1_RX_Buffer_Reset();
+            DMA1_Channel15_Reload();
+            subroutine_flag &= ~DMA1_ERROR;
+        }
     }
+    delay_ms(1000);
+    LL_GPIO_TogglePin(GPIOA, LL_GPIO_PIN_5);
 }
 
 /* Interrupt handlers here */
@@ -61,12 +90,35 @@ void SysTick_Handler(void) {
     LL_GPIO_TogglePin(GPIOA, LL_GPIO_PIN_8);
 }
 
+void modbus_routine(void) {
+    if (MODBUS_RTU_ERR_SUCCESS !=
+        modbusRtu_AddressValidation(usart1_rx_dma_buffer[SLAVE_ADDRESS])) {
+#if (DEBUG_CONSOLE_EN > 0u)
+        char debug_msg[DBUG_MSG_LEN];
+        snprintf(debug_msg, DBUG_MSG_LEN, "SlaveAddr:%#x\n\r", usart1_rx_dma_buffer[SLAVE_ADDRESS]);
+        debug_console("Not my address, discard the frame!\r\n");
+#endif
+    } else {
+        modbusRtu_ParseRequest(usart1_rx_dma_buffer);
+#if (DEBUG_CONSOLE_EN > 0u)
+        debug_console("My address, Run Modbus_ParseRequest()!\r\n");
+#endif
+    }
+    // TBD - start usart1_watchdog(USART1_RX_Buffer_Reset)
+    subroutine_flag &= ~MODBUS_REQUEST;  // disable the flag
+    u1tc_flag = false;
+    u1ts_flag = false;
+    USART1_RX_Buffer_Reset();
+    DMA1_Channel15_Reload();
+}
+
 /**
  * \brief   DMA1 channel5 interrupt handler for USART1 RX
  * \author  Siyuan Xu,
  */
 void DMA1_Channel5_IRQHandler(void) {
     /* Check half-transfer complete interrupt */
+    // TBD-REMOVE HALF-TRANSFER INTERRUPT
     if (DMA1->ISR & DMA_ISR_HTIF5) {
 #if (DEBUG_CONSOLE_EN > 0u)
         debug_console("USART1 DMA half-transfer interrupt!\r\n");
@@ -79,26 +131,9 @@ void DMA1_Channel5_IRQHandler(void) {
 #if (DEBUG_CONSOLE_EN > 0u)
         debug_console("USART1 DMA transfer-complete interrupt!\r\n");
 #endif
-        u1tcFlag = true;
-        DMA1->IFCR |= DMA_IFCR_CTCIF5; /*!< Channel 5 Transfer Complete clear */
-        if (MODBUS_RTU_ERR_SUCCESS !=
-            modbusRtu_AddressValidation(usart1_rx_dma_buffer[SLAVE_ADDRESS])) {
-#if (DEBUG_CONSOLE_EN > 0u)
-            char debug_msg[DBUG_MSG_LEN];
-            snprintf(debug_msg, DBUG_MSG_LEN, "SlaveAddr:%#x\n\r",
-                     usart1_rx_dma_buffer[SLAVE_ADDRESS]);
-            debug_console("Not my address, discard the frame!\r\n");
-#endif
-        } else {
-            modbusRtu_ParseRequest(usart1_rx_dma_buffer);
-#if (DEBUG_CONSOLE_EN > 0u)
-            debug_console("My address, Run Modbus_ParseRequest()!\r\n");
-#endif
-        }
-        // TBD - start usart1_watchdog(USART1_RX_Buffer_Reset)
-        USART1_RX_Buffer_Reset();
-        DMA1_Channel15_Reload();
-        // u1tcFlag = false;
+        u1tc_flag = true;
+        subroutine_flag |= MODBUS_REQUEST;  // enable the flag
+        DMA1->IFCR |= DMA_IFCR_CTCIF5;      /*!< Channel 5 Transfer Complete clear */
     }
 }
 
@@ -115,7 +150,7 @@ void USART1_IRQHandler(void) {
 #if (DEBUG_CONSOLE_EN > 0u)
         debug_console("USART1 Read Data Register Not Empty!\r\n");
 #endif
-        u1tsFlag = true;
+        u1ts_flag = true;
     }
     /* Check for IDLE line interrupt */
     if (status & USART_SR_IDLE) {
@@ -123,10 +158,10 @@ void USART1_IRQHandler(void) {
         debug_console("USART1 Idle-line interrupt!\r\n");
 #endif
         // TBD - Stop usart1_watchdog
-        if (u1tcFlag == true) {
-            u1tcFlag = false;
-            USART1_RX_Buffer_Reset();
-            DMA1_Channel15_Reload();
+        if (u1tc_flag == true) {
+            // u1tc_flag = false;
+            // USART1_RX_Buffer_Reset();
+            // DMA1_Channel15_Reload();
             data = USART1->DR; /* Clear IDLE line flag */
         } else {
             // Error condition when USART1 DMA transmit complete flag is false and IDLE line
@@ -134,22 +169,21 @@ void USART1_IRQHandler(void) {
 #if (DEBUG_CONSOLE_EN > 0u)
             debug_console(
                 "Error!"
-                "IDLE line detected, but transmition complete flag is FALSE!\n\r"
+                "IDLE line detected, but dma transmition complete flag is FALSE!\n\r"
                 "Received modbus frame length is lesser than expected.\n\r");
 #endif
-            USART1_RX_Buffer_Reset();
-            DMA1_Channel15_Reload();
+            subroutine_flag |= DMA1_ERROR;
             data = USART1->DR; /* Clear IDLE line flag */
         }
     }
-    /* Check Tranmission complete intrrupt */
+    /* Check Tranmission complete interrupt */
     if (status & USART_SR_TC) {
-        if (usart1_rx_dma_buffer[0] != NULL) {
-            USART1_RX_Buffer_Reset();
-            DMA1_Channel15_Reload();
+
+        if (!u1tc_flag && usart1_rx_dma_buffer[0] != 0) {
+            subroutine_flag |= DMA1_ERROR;
 #if (DEBUG_CONSOLE_EN > 0u)
             debug_console(
-                "ERROR! Transmition complete. But USART1_RX buffer is not empty." 
+                "ERROR! Transmition complete. But USART1_RX buffer is not empty."
                 "Received modbus frame length is greater than expected.\r\n");
 #endif
         }
@@ -161,13 +195,13 @@ void USART1_IRQHandler(void) {
  * \author  Siyuan xu, e2101066@edu.vamk.fi, Feb.2023
  */
 void DMA1_Channel6_IRQHandler(void) {
+    // TBD REMOVE DMA1_Channel6
     /* Check half-transfer complete interrupt */
     if (DMA1->ISR & DMA_ISR_HTIF6) {
 #if (DEBUG_CONSOLE_EN > 0u)
         debug_console("USART2 DMA half-transfer interrupt!\r\n");
 #endif
         DMA1->IFCR |= DMA_IFCR_CHTIF6; /*!< Channel 6 Half Transfer clear */
-
         USART2_send_data(usart2_rx_dma_buffer, USART2_RX_DMA_BUFFER_SIZE);
     }
 
