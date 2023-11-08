0da11e41e5a318847ab76ccd19802bdce8f76031
D3SXX <9647752d@gmail.com>
2023-11-08 14:51:01 +0200

    Update Actions & Improve compatibility with linux

--- test/crc_test.c
+++ test/crc_test.c
@@ -1,4 +1,4 @@
-#include "CRC.h"
+#include "crc.h"
 
 #include <unity.h>
 
@@ -20,7 +20,7 @@ void test_crc16_positive(void) {
 void test_crc16_negative(void) {
     uint8_t  test_data[6] = {05, 04, 00, 01, 00, 01};
     uint16_t crc16        = CRC16(test_data, 6);
-    uint16_t expected_crc16_result = ((uint16_t)142 << 8) + ((uint16_t)96);
+    uint16_t expected_crc16_result = ((uint16_t)142 << 8) + ((uint16_t)90);
     TEST_ASSERT_NOT_EQUAL(crc16, expected_crc16_result);
 }
 
