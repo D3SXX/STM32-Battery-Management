6ad1def631c741b5fc0e9838842f641269020dbd
Siyuan Xu <siyuan.xu@outlook.com>
2023-11-13 16:03:01 +0200

    fixed current and temperature convertion bugs

--- lib/adc/adc.c
+++ lib/adc/adc.c
@@ -1,7 +1,5 @@
 #include "adc.h"
 
-#include "moc_adc_read.h"
-
 /**
  * \brief Initialise ADC settings.
  * \author siyuan xu, e2101066@edu.vamk.fi, Nov.2023
@@ -60,7 +58,7 @@ uint16_t adc_read(const uint8_t adc_channel) {
  */
 uint16_t adc_convert_cell_voltage(uint16_t (*adc_read)(const uint8_t adc_channel)) {
     uint16_t cell_adc  = (*adc_read)(ADC_INPUT_CELL_VOLTAGE);
-    double   cell_volt = (double)cell_adc / 4095 * 4.125;  // Cell Voltage range 0 - 4.125V
+    double   cell_volt = (double)cell_adc * 4.125 / 4095;  // Cell Voltage range 0 - 4.125V
     return (uint16_t)(cell_volt * 1000);                   // accuracy in 1 mV.
 }
 
@@ -71,10 +69,11 @@ uint16_t adc_convert_cell_voltage(uint16_t (*adc_read)(const uint8_t adc_channel
  * \author siyuan xu, e2101066@edu.vamk.fi, Nov.2023
  * \details This function converts 12 bit ADC value into 0 - 41250mA.
  */
-uint16_t adc_convert_batt_current(uint16_t (*adc_read)(const uint8_t adc_channel)) {
-    uint16_t current_adc  = (*adc_read)(ADC_INPUT_BATT_CURRENT);
-    double   cell_current = (double)current_adc / 4095 * 41.25;  // current sensing range 0 - 41.25A
-    return (uint16_t)(cell_current * 1000);                      // accuracy in 1 mA.
+uint16_t adc_convert_batt_current(uint16_t adc_value) {
+    double shunt_volt = (double)adc_value * 4.125 / 4095;  // current sensing range 0 - 4.125V
+    double shunt_current_double = shunt_volt * 10;
+    int    shunt_current_int    = (int)(shunt_current_double * 1000);  // accuracy in 1 mA.
+    return (uint16_t)(shunt_current_int);
 }
 
 /**
@@ -85,10 +84,11 @@ uint16_t adc_convert_batt_current(uint16_t (*adc_read)(const uint8_t adc_channel
  * \details This function converts 12 bit ADC value into -50 - 150C.
  */
 uint16_t adc_convert_batt_temp(uint16_t (*adc_read)(const uint8_t adc_channel)) {
-    uint16_t temp_adc      = (*adc_read)(ADC_INPUT_BATT_TEMP);
-    uint16_t reference_adc = 1240;  // 1V reference voltage on LM35, need to be measured still.
-    double   temp_celcius  = (temp_adc - reference_adc) / 4095 * 3.3;  //
-    int16_t  temp_int      = (int16_t)(temp_celcius * 10);             // accuracy in 0.1 Celcius
-    uint16_t encoded_temp  = (uint16_t)temp_int;  // encode temp to uint16_t for modbus register
+    uint16_t temp_adc = (*adc_read)(ADC_INPUT_BATT_TEMP);
+    uint16_t reference_adc =
+        LM35_ADC_REF;  // 1V reference voltage on LM35, need to be measured still.
+    double   temp_celcius = (temp_adc - reference_adc) * 3.3 * 100 / 4095;  //
+    int      temp_int     = (int)(temp_celcius * 10);  // accuracy in 0.1 Celcius
+    uint16_t encoded_temp = (uint16_t)temp_int;  // encode temp to uint16_t for modbus register
     return encoded_temp;
 }
