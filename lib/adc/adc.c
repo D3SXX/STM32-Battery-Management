131fc386006d05f8910272d0c693893921c313cb
Siyuan Xu <siyuan.xu@outlook.com>
2023-11-13 12:22:48 +0200

    added adc library

--- lib/adc/adc.c
+++ lib/adc/adc.c
@@ -1,46 +1,94 @@
 #include "adc.h"
 
-#include "stm32l1xx.h"
 #include "moc_adc_read.h"
 
+/**
+ * \brief Initialise ADC settings. 
+ * \author siyuan xu, e2101066@edu.vamk.fi, Nov.2023
+ * \details ADC channel 0, PA0 (A0) in single conversion mode.
+ */
 void adc_init(void) {
-    // RCC->AHBENR |= (RCC_APB2ENR_ADC1EN);
-    // RCC_
-    // RCC->CCIPR &= ~(RCC_CCIPR_ADCSEL);
-    // RCC->CCIPR |= (3 << RCC_CCIPR_ADCSEL_Pos);
+    /* PA0 (A0) -- MUX (CH0)
+       PA1 (A1) -- Temp_P ?
+       PA4 (A2) -- Temp_N ?
+       PC1 (A4) -- Current ?
+    */
+    RCC->AHBENR |= RCC_AHBENR_GPIOAEN;              // Enable GPIOA
+    GPIOA->MODER |= (GPIO_MODE_ANALOG << PA0 * 2);  // Set PA0 to ANALOG mode
+    // GPIOA->MODER |= (GPIO_MODE_ANALOG << PA1 * 2);
+    // GPIOA->MODER |= (GPIO_MODE_ANALOG << PA4 * 2);
 
-    // GPIOA->MODER |= (0x3 << (1 * 2));
+    RCC->AHBENR |= RCC_AHBENR_GPIOCEN;  // Enable GPIOC
+    GPIOB->MODER |= (GPIO_MODE_ANALOG << PC1 * 2);
+
+    /* ADC configurations */
+    RCC->APB2ENR |= (RCC_APB2ENR_ADC1EN);         // Enable ADC1 clock.
+    ADC1->CR2 &= ~ADC_CR2_CONT;                   // Set Single Conversion.
+    ADC1->SMPR3 |= (0x07 << ADC_SMPR3_SMP0_Pos);  // Set ADC->CH0 sample time to max 384 cycles
+    // ADC1->SMPR3 |= (0x07 << ADC_SMPR3_SMP1_Pos) |   //Set ADC->CH1 and CH2 as well..
+    //                 (0x07 << ADC_SMPR3_SMP2_Pos)
+    ADC1->CR1 &= 0x03 << ADC_CR1_RES_Pos;  // Set resolution to 12 bit
 }
 
-void inline adc_power_up() {}
-
-uint16_t adc_read(const int pin) {
-    // // ADC1->CR2 &= ~(ADC_CR2_ADON);    //Disable ADC
-    // // Enable the ADC voltage regulator.
-    // ADC1->CR2 |= (ADC_CR2_ADON);
-    // // Delay for a handful of microseconds.
-    // delay_cycles(100);
-    // // Calibrate the ADC if necessary.
-    // if (perform_calibration) {
-    //     ADC1->CR |= (ADC_);
-    //     while (ADC1->CR & ADC_CR_ADCAL) {
-    //     };
-    // }
-    return 1024;
+/**
+ * \brief Read single conversion result from selected ADC channel. 
+ * \param[in] adc_channel The selected channel for conversion.
+ * \return 12 bit ADC value.
+ * \author siyuan xu, e2101066@edu.vamk.fi, Nov.2023
+ * \details The selected channel should be already enabled.
+ */
+uint16_t adc_read(const uint8_t adc_channel) {
+    int result = 0;
+    ADC1->CR2 &= ~(ADC_CR2_ADON);  // Make sure ADC is diabled.
+    // Enable the ADC voltage regulator.
+    ADC1->CR2 |= (ADC_CR2_ADON);
+    ADC1->SQR5 = adc_channel;             // Set conversion sequence 1 to ch0.
+    ADC1->CR2 |= ADC_CR2_ADON;     // Set bit 0, ADC on/off (1=on, 0=off).
+    ADC1->CR2 |= ADC_CR2_SWSTART;  // start conversion.
+    while (!(ADC1->SR & 2)) {
+    }                   // wait for conversion complete
+    result = ADC1->DR;  // read conversion result
+    return result;
 }
 
-void adc_read_current(_Atomic uint16_t* data) { 
-    // *data = adc_read(PIN_CURRENT);
-    *data = moc_adc_read_batt_current();
+/**
+ * \brief Set return value for moc_adc_read(). 
+ * \param[in] adc_read The function for reading ADC values.
+ * \return The cell voltage value.
+ * \author siyuan xu, e2101066@edu.vamk.fi, Nov.2023
+ * \details This function converts 12 bit ADC value into 0 - 4125mV.
+ */
+uint16_t adc_convert_cell_voltage(uint16_t *adc_read(const int adc_pin)) {
+    uint16_t cell_adc  = (*adc_read)(ADC_INPUT_CELL_VOLTAGE);
+    double   cell_volt = (double)cell_adc / 4095 * 4.125;  // Cell Voltage range 0 - 4.125V
+    return (uint16_t)(cell_volt * 1000);                   // accuracy in 1 mV.
 }
-void adc_read_temperature(_Atomic uint16_t* data) { 
-    // *data = adc_read(PIN_TEMP); 
-    *data = moc_adc_read_batt_temp();
+
+/**
+ * \brief Set return value for moc_adc_read(). 
+ * \param[in] adc_read The function for reading ADC values.
+ * \return The battery charging current value.
+ * \author siyuan xu, e2101066@edu.vamk.fi, Nov.2023
+ * \details This function converts 12 bit ADC value into 0 - 41250mA.
+ */
+uint16_t adc_convert_batt_current(uint16_t *adc_read(const int adc_pin)) {
+    uint16_t current_adc  = (*adc_read)(ADC_INPUT_BATT_CURRENT);
+    double   cell_current = (double)current_adc / 4095 * 41.25;  // current sensing range 0 - 41.25A
+    return (uint16_t)(cell_current * 1000);                      // accuracy in 1 mA.
 }
-void adc_read_volts(_Atomic uint16_t* data) {
-    for (int i = 0; i < 4; i++) {
-        // mux_set(i + 1);
-        // *(data + i) = adc_read(PIN_VOLT);
-        *(data + i) = moc_adc_read_cell_voltage();
-    }
+
+/**
+ * \brief Set return value for moc_adc_read(). 
+ * \param[in] adc_read The function for reading ADC values.
+ * \return The cell voltage value.
+ * \author siyuan xu, e2101066@edu.vamk.fi, Nov.2023
+ * \details This function converts 12 bit ADC value into -50 - 150C.
+ */
+uint16_t adc_convert_batt_temp(uint16_t *adc_read(const int adc_pin)) {
+    uint16_t temp_adc      = (*adc_read)(ADC_INPUT_BATT_TEMP);
+    uint16_t reference_adc = 1240;  // 1V reference voltage on LM35, need to be measured still.
+    double   temp_celcius  = (temp_adc - reference_adc) / 4095 * 3.3;  //
+    int16_t  temp_int      = (int16_t)(temp_celcius * 10);             // accuracy in 0.1 Celcius
+    uint16_t encoded_temp  = (uint16_t)temp_int;  // encode temp to uint16_t for modbus register
+    return encoded_temp;
 }
--- lib/adc/adc.h
+++ lib/adc/adc.h
@@ -1,15 +1,29 @@
 #ifndef ADC_H
 #define ADC_H
-#include <stdatomic.h>
 #include <stdint.h>
 
-#define PIN_CURRENT 1
-#define PIN_TEMP    2
-#define PIN_VOLT    3
+#include "stm32l1xx.h"
 
-void     adc_read_current(_Atomic uint16_t* data);
-void     adc_read_temperature(_Atomic uint16_t* data);
-void     adc_read_volts(_Atomic uint16_t* data);
-uint16_t adc_read(const int pin);
+#define GPIO_MODE_INPUT        0x00U /*!< Select input mode */
+#define GPIO_MODE_OUTPUT       0x01U /*!< Select output mode */
+#define GPIO_MODE_ALTERNATE    0x02U /*!< Select alternate function mode */
+#define GPIO_MODE_ANALOG       0x03U /*!< Select analog mode */
+#define PA0                    0U
+#define PA1                    1U
+#define PA4                    4U
+#define PC1                    1U
+#define ADC_INPUT_BATT_CURRENT PA0
+#define ADC_INPUT_BATT_TEMP    PA0
+#define ADC_INPUT_CELL_VOLTAGE PA0
+#define PIN_CURRENT            1
+#define PIN_TEMP               2
+#define PIN_VOLT               3
+
 void     adc_init(void);
+uint16_t adc_read(const uint8_t adc_ch);
+uint16_t adc_convert_cell_voltage(uint16_t *adc_read(const int adc_pin));
+uint16_t adc_convert_batt_current(uint16_t *adc_read(const int adc_pin));
+uint16_t adc_convert_batt_temp(uint16_t *adc_read(const int adc_pin));
+void inline adc_power_up() { ADC1->CR2 &= ~(ADC_CR2_ADON); }
+void inline adc_power_down() { ADC1->CR2 |= (ADC_CR2_ADON); }
 #endif
