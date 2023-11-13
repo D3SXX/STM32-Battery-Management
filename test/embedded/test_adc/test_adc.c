90c63a20a02fe92e55da6dbbe3163f3d9dadba7d
Siyuan Xu <siyuan.xu@outlook.com>
2023-11-13 16:02:07 +0200

    better testing data for temperature

--- test/embedded/test_adc/test_adc.c
+++ test/embedded/test_adc/test_adc.c
@@ -96,14 +96,14 @@ void test_adc_convert_temperature_edge_max(void){
     int16_t temp_int = 0;
     moc_adc_set(LM35_ADC_MAX);
     temp_int = (int16_t)adc_convert_batt_temp(moc_adc_read);
-    TEST_ASSERT_EQUAL((int16_t)15005, temp_int);
+    TEST_ASSERT_EQUAL((int16_t)1500, temp_int);
 }
 
 void test_adc_convert_temperature_edge_min(void){
     int16_t temp_int = 0;
     moc_adc_set(LM35_ADC_MIN);
     temp_int = (int16_t)adc_convert_batt_temp(moc_adc_read);
-    TEST_ASSERT_EQUAL((int16_t)-5495, temp_int);
+    TEST_ASSERT_EQUAL((int16_t)-549, temp_int);
 }
 
 static inline void LED2_init(void) {
@@ -162,39 +162,3 @@ int main(int argc, char **argv) {
 
 void SysTick_Handler(void) { HAL_IncTick(); }
 
-// #include <unity.h>
-
-// #include "../unity_config.h"
-// // #include "adc.h"
-// // #include "moc_adc_read.h"
-// // #include "stm32l1xx.h"
-
-// void setUp(void) {
-//     // set stuff up here
-// }
-
-// void tearDown(void) {
-//     // clean stuff up here
-// }
-
-// void test_adc_convert_current_positive(void) {
-//     // uint16_t adc_value = 0;
-//     uint16_t current = 0;
-//     // moc_adc_set(4095);
-//     // adc_value = moc_adc_read((uint8_t)0xff);
-//     // current = adc_convert_batt_current(adc_value);
-//     TEST_ASSERT_EQUAL((uint16_t)41250, current);
-// }
-
-// int main(int argc, char **argv) {
-//     HAL_Init();       // initialize the HAL library
-//     HAL_Delay(2000);  // service delay
-
-//     UNITY_BEGIN();
-//     RUN_TEST(test_adc_convert_current_positive);
-//     HAL_Delay(500);
-//     UNITY_END();
-
-//     while (1) {
-//     }
-// }
