b8eaab4f51ed2abd8dd193b4f989cdb31d44df31
Siyuan Xu <siyuan.xu@outlook.com>
2023-11-15 12:05:34 +0200

    refactored adc for better testing

--- lib/adc/adc.h
+++ lib/adc/adc.h
@@ -2,37 +2,12 @@
 #define ADC_H
 #include <stdint.h>
 
+#include "adc_settings.h"
+#include "adc_utils.h"
 #include "stm32l1xx.h"
+static inline void adc_power_up(void) { ADC1->CR2 &= ~(ADC_CR2_ADON); }
+static inline void adc_power_down(void) { ADC1->CR2 |= (ADC_CR2_ADON); }
 
-#define GPIO_MODE_INPUT         0x00U /*!< Select input mode */
-#define GPIO_MODE_OUTPUT        0x01U /*!< Select output mode */
-#define GPIO_MODE_ALTERNATE     0x02U /*!< Select alternate function mode */
-#define GPIO_MODE_ANALOG        0x03U /*!< Select analog mode */
-#define PA0                     0U
-#define PA1                     1U
-#define PA4                     4U
-#define PC1                     1U
-#define ADC_INPUT_BATT_CURRENT  PA0
-#define ADC_INPUT_BATT_TEMP     PA0
-#define ADC_INPUT_CELL_VOLTAGE  PA0
-#define PIN_CURRENT             1
-#define PIN_TEMP                2
-#define PIN_VOLT                3
-#define LM35_VOLT_REF           (double)1.0
-#define LM35_VOLT_MAX           (double)(LM35_VOLT_REF + 1.5)
-#define LM35_VOLT_MIN           (double)(LM35_VOLT_REF - 0.55)
-#define LM35_VOLT_TO_ADC(volt)  (volt * 4095 / 3.3)
-#define LM35_ADC_REF            (uint16_t) LM35_VOLT_TO_ADC(LM35_VOLT_REF)
-#define LM35_ADC_MAX            (uint16_t) LM35_VOLT_TO_ADC(LM35_VOLT_MAX)
-#define LM35_ADC_MIN            (uint16_t) LM35_VOLT_TO_ADC(LM35_VOLT_MIN)
-#define LM35_VOLT_TO_TEMP(volt) ((volt - LM35_VOLT_REF) / 0.01)
-
-#define SHUNT_RESISTANCE        (double)0.01
 void     adc_init(void);
 uint16_t adc_read(const uint8_t adc_ch);
-uint16_t adc_convert_cell_voltage(uint16_t (*adc_read)(const uint8_t adc_channel));
-uint16_t adc_convert_batt_current(const uint16_t adc_value);
-uint16_t adc_convert_batt_temp(uint16_t (*adc_read)(const uint8_t adc_channel));
-void inline adc_power_up() { ADC1->CR2 &= ~(ADC_CR2_ADON); }
-void inline adc_power_down() { ADC1->CR2 |= (ADC_CR2_ADON); }
 #endif
---
+++ lib/adc/adc_utils.c
@@ -0,0 +1,44 @@
+#include "adc_utils.h"
+/**
+ * \brief Set return value for moc_adc_read().
+ * \param[in] adc_read The function for reading ADC values.
+ * \return The cell voltage value.
+ * \author siyuan xu, e2101066@edu.vamk.fi, Nov.2023
+ * \details This function converts 12 bit ADC value into 0 - 4125mV.
+ */
+uint16_t adc_convert_cell_voltage(uint16_t (*adc_read)(const uint8_t adc_channel)) {
+    uint16_t cell_adc  = (*adc_read)(ADC_INPUT_CELL_VOLTAGE);
+    double   cell_volt = (double)cell_adc * 4.125 / 4095;  // Cell Voltage range 0 - 4.125V
+    return (uint16_t)(cell_volt * 1000);                   // accuracy in 1 mV.
+}
+
+/**
+ * \brief Set return value for moc_adc_read().
+ * \param[in] adc_read The function for reading ADC values.
+ * \return The battery charging current value.
+ * \author siyuan xu, e2101066@edu.vamk.fi, Nov.2023
+ * \details This function converts 12 bit ADC value into 0 - 41250mA.
+ */
+uint16_t adc_convert_batt_current(uint16_t adc_value) {
+    double shunt_volt = (double)adc_value * 4.125 / 4095;  // current sensing range 0 - 4.125V
+    double shunt_current_double = shunt_volt * 10;
+    int    shunt_current_int    = (int)(shunt_current_double * 1000);  // accuracy in 1 mA.
+    return (uint16_t)(shunt_current_int);
+}
+
+/**
+ * \brief Set return value for moc_adc_read().
+ * \param[in] adc_read The function for reading ADC values.
+ * \return The cell voltage value.
+ * \author siyuan xu, e2101066@edu.vamk.fi, Nov.2023
+ * \details This function converts 12 bit ADC value into -50 - 150C.
+ */
+uint16_t adc_convert_batt_temp(uint16_t (*adc_read)(const uint8_t adc_channel)) {
+    uint16_t temp_adc = (*adc_read)(ADC_INPUT_BATT_TEMP);
+    uint16_t reference_adc =
+        LM35_ADC_REF;  // 1V reference voltage on LM35, need to be measured still.
+    double   temp_celcius = (temp_adc - reference_adc) * 3.3 * 100 / 4095;  //
+    int      temp_int     = (int)(temp_celcius * 10);  // accuracy in 0.1 Celcius
+    uint16_t encoded_temp = (uint16_t)temp_int;  // encode temp to uint16_t for modbus register
+    return encoded_temp;
+}
